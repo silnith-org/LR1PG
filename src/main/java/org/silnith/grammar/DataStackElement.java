a2ebae159213b53f39e07e30e39ff87973dddbc2
silnith <silnith@gmail.com>
2020-05-01 06:48:53 -0700

    Whitespace

--- src/main/java/org/silnith/grammar/DataStackElement.java
+++ src/main/java/org/silnith/grammar/DataStackElement.java
@@ -2,21 +2,21 @@ package org.silnith.grammar;
 
 // TODO: parameterize this type
 public class DataStackElement {
-	
-	final Object abstractSyntaxTree;
-	
-	public DataStackElement(final Object abstractSyntaxTree) {
-		super();
-		this.abstractSyntaxTree = abstractSyntaxTree;
-	}
-
-	public Object getAbstractSyntaxTreeElement() {
-		return abstractSyntaxTree;
-	}
-
-	@Override
-	public String toString() {
-		return "[" + abstractSyntaxTree + "]";
-	}
-
+    
+    final Object abstractSyntaxTree;
+    
+    public DataStackElement(final Object abstractSyntaxTree) {
+        super();
+        this.abstractSyntaxTree = abstractSyntaxTree;
+    }
+    
+    public Object getAbstractSyntaxTreeElement() {
+        return abstractSyntaxTree;
+    }
+    
+    @Override
+    public String toString() {
+        return "[" + abstractSyntaxTree + "]";
+    }
+    
 }
