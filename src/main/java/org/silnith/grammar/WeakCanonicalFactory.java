a5ed1ddf9a3313bc99438af0bbc24cdaee6a5466
kent <silnith@gmail.com>
2020-05-01 10:05:58 -0700

    Add a version of the canonical factory that uses weak references.
    Not making this the primary implementation yet until I prove it is
    correct.

---
+++ src/main/java/org/silnith/grammar/WeakCanonicalFactory.java
@@ -0,0 +1,70 @@
+package org.silnith.grammar;
+
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class WeakCanonicalFactory<T> {
+    
+    private final Map<T, WeakReference<T>> canonicalInstance;
+
+    private long callCount;
+
+    public WeakCanonicalFactory() {
+        super();
+        this.canonicalInstance = new WeakHashMap<T, WeakReference<T>>();
+        this.callCount = 0;
+    }
+    
+    public synchronized T valueOf(final T t) {
+        callCount++;
+
+        /*
+         * As long as the strong reference t exists, this weak reference will not vanish.
+         */
+        final WeakReference<T> newWeakReference = new WeakReference<>(t);
+        
+        final WeakReference<T> existingWeakReference = canonicalInstance.get(t);
+        if (existingWeakReference == null) {
+            /*
+             * There was no existing entry.  Create one.
+             */
+            final WeakReference<T> previousWeakReference = canonicalInstance.put(t, newWeakReference);
+            
+            /*
+             * Because this method is synchronized, it is not possible for another thread to preemptively insert a new
+             * canonical instance.
+             */
+            assert previousWeakReference == null;
+            
+            return t;
+        } else {
+            /*
+             * There was an existing entry, check if it is still valid.
+             */
+            final T existingT = existingWeakReference.get();
+            if (existingT == null) {
+                /*
+                 * The weak reference in the canonical map vanished.  Continue with the newly-created one.
+                 */
+                canonicalInstance.put(t, newWeakReference);
+
+                return t;
+            } else {
+                /*
+                 * The weak reference in the canonical map is valid, return it as a strong reference.
+                 */
+                return existingT;
+            }
+        }
+    }
+    
+    public synchronized long getInstanceCount() {
+        return canonicalInstance.size();
+    }
+    
+    public synchronized long getCallCount() {
+        return callCount;
+    }
+    
+}
