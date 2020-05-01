da808d065eef0cbacff3c812189f62cbe67605d0
silnith <silnith@gmail.com>
2020-04-30 18:09:29 -0700

    Encapsulate tracking change for each production.

--- src/main/java/org/silnith/grammar/Grammar.java
+++ src/main/java/org/silnith/grammar/Grammar.java
@@ -367,15 +367,17 @@ public class Grammar<T extends TerminalSymbol> {
                 
                 for (final Production production : productions) {
                     final List<Symbol> symbols = production.getSymbols();
+                    boolean changedByProduction = false;
                     for (final Symbol symbol : symbols) {
                         final Set<T> firstSetForSymbolInProduction = getFirstSet(symbol);
                         final boolean addedElementsToFirstSet = firstSetForLeftHandSide.addAll(firstSetForSymbolInProduction);
-                        changed = addedElementsToFirstSet || changed;
+                        changedByProduction = addedElementsToFirstSet || changedByProduction;
                         
                         if (!nullable.contains(symbol)) {
                             break;
                         }
                     }
+                    changed = changedByProduction || changed;
                 }
             }
         } while (changed);
