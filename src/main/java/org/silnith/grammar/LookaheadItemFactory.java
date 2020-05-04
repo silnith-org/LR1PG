4eb037023671d76a6263a0d02847c5cbdf0ae4c6
kent <silnith@gmail.com>
2020-05-04 14:33:31 -0700

    Remove default constructor.

--- src/main/java/org/silnith/grammar/LookaheadItemFactory.java
+++ src/main/java/org/silnith/grammar/LookaheadItemFactory.java
@@ -4,10 +4,6 @@ import java.util.Set;
 
 public class LookaheadItemFactory<T extends TerminalSymbol> extends WeakCanonicalFactory<LookaheadItem<T>> {
     
-    public LookaheadItemFactory() {
-        super();
-    }
-    
     public LookaheadItem<T> createInstance(final Item item, final Set<T> lookaheadSet) {
         return valueOf(new LookaheadItem<>(item, lookaheadSet));
     }
