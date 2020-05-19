91d64034f3c475414eb6ca2fb00ea5987abd5c2c
silnith <silnith@gmail.com>
2020-05-19 01:02:39 -0700

    Remove unused member variable.

--- src/main/java/org/silnith/grammar/AbstractAction.java
+++ src/main/java/org/silnith/grammar/AbstractAction.java
@@ -2,14 +2,11 @@ package org.silnith.grammar;
 
 abstract class AbstractAction<T extends TerminalSymbol> implements Action<T> {
 
-    private final Parser<T> parser;
-
     public AbstractAction(final Parser<T> parser) {
         super();
         if (parser == null) {
             throw new IllegalArgumentException();
         }
-        this.parser = parser;
     }
 
 }
